e7c63938a0490fa2f7829c015275556e59d70d16
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-10-05 13:24:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 # Lista de usuários permitidos
-allowed_users = ["user1", "user2", "kenovenas"]  # Insira os usuários permitidos aqui
+allowed_users = ["user1", "user2", "kenovenas"]
 
-# Rota para a autenticação do usuário
 @app.route('/login', methods=['POST'])
 def authenticate():
     username = request.json.get('username')
     if username in allowed_users:
         return jsonify({"authenticated": True}), 200
-    else:
-        return jsonify({"authenticated": False}), 401
+    return jsonify({"authenticated": False}), 401
 
 if __name__ == '__main__':
     app.run(debug=True)
