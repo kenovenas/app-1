2880dc9b180084aad0e3659ba94c0d3243023b70
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-10-23 20:48:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Habilita CORS para todas as rotas
 
 # Configurações do usuário autorizado
 AUTHORIZED_USER = "nome"  # Altere para o nome de usuário desejado
