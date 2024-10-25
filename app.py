c82f81abb61d8cbac0677a55d65d381843e24610
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-10-25 16:47:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,34 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas as rotas
 
-# Lista de usuários autorizados (substitua por um banco de dados em produção)
-usuarios_autorizados = ["fiel",
-                "ok6675",
-                 "ok3286",
-                 "ok1390",
-                 "zr1",
-                 "mro",
-                 "hmd",
-                 "mrclm",
-                 "mxwll",
-                 "nmmr",
-                        "mts",
-                        "jncmps",
-                        "dnln",
-                       "ok1698",
-                        "ok0091",
-                        "ok0908",
-                        "ok2508",
-                        "ok2956",
-                        "ok1203",
-                        "ok9019",
-                        "ok4004",
-                        "ok1999",
-                        "ok1982",
-                       ]
+# Lista de usuários autorizados
+usuarios_autorizados = [
+    "fiel", "ok6675", "crtntt", "ok3286", "ok1390", "zr1", "nbsbt", "mxchk",
+    "pdrrm", "mro", "hmd", "mrclm", "mxwll", "nmmr", "mts", "jncmps", "dnln",
+    "ok1698", "ok0091", "ok0908", "ok2508", "ok2956", "ok1203"
+]
 
 @app.route('/validar_usuario', methods=['POST'])
 def validar_usuario():
     data = request.get_json()
     usuario = data.get('usuario')
 
+    # Verifica se o usuário é autorizado
     if usuario in usuarios_autorizados:
+        # Imprime no console o usuário autorizado e o horário da requisição
+        print(f"[{datetime.now()}] Usuário autorizado: {usuario}")
         return jsonify({'autorizado': True}), 200
     else:
+        # Imprime no console a tentativa de acesso não autorizada
+        print(f"[{datetime.now()}] Tentativa de acesso negada para o usuário: {usuario}")
         return jsonify({'autorizado': False}), 403  # Forbidden
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Certifique-se de que a porta está configurada para ser dinâmica no Render
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
