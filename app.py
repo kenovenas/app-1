f7348bba64ebd0f8e2110f01b85e99c214e44cea
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-11-01 18:53:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,37 +10,15 @@ logging.basicConfig(level=logging.INFO)
 
 # Lista de usuários autorizados
 usuarios_autorizados = [
-    "ok6675",
-                 "ok3286",
-                 "ok1390",
-                 "zr1",
-                 "mro",
-                 "mrclm",
-                 "mxwll",
-                 "nmmr",
-    "nbsbt",
-                        "mts",
-                        "jncmps",
-                        "dnln",
-                       "ok1698",
-                        "ok0091",
-                        "ok0908",
-                        "ok2508",
-                        "ok2956",
-                        "ok1203",
-                        "ok9019",
-                        "ok4004",
-                        "ok1999",
-                        "ok1982",
-                         "ok0198",
-    "ok1001",
-    "ok2090",
-    "ok0902",
-    "ok1516",
-    "ok1119",
-    "ok2324",
-    "admin"
+    "ok6675", "ok3286", "ok1390", "zr1", "mro", "mrclm", "mxwll", 
+    "nmmr", "nbsbt", "mts", "jncmps", "dnln", "ok1698", "ok0091", 
+    "ok0908", "ok2508", "ok2956", "ok1203", "ok9019", "ok4004", 
+    "ok1999", "ok1982", "ok0198", "ok1001", "ok2090", "ok0902", 
+    "ok1516", "ok1119", "ok2324", "admin"
 ]
+
+# Dicionário para rastrear visitas dos usuários
+contador_visitas = {usuario: 0 for usuario in usuarios_autorizados}
 
 @app.route('/validar_usuario', methods=['POST'])
 def validar_usuario():
@@ -48,8 +26,11 @@ def validar_usuario():
     usuario = data.get('usuario')
 
     if usuario in usuarios_autorizados:
-        logging.info(f"Usuário autorizado: {usuario}")  # Log do nome do usuário
-        return jsonify({'autorizado': True}), 200
+        # Incrementa o contador de visitas
+        contador_visitas[usuario] += 1
+        visitas = contador_visitas[usuario]
+        logging.info(f"Usuário autorizado: {usuario} - Visitas: {visitas}")  # Log do nome do usuário e contagem de visitas
+        return jsonify({'autorizado': True, 'visitas': visitas}), 200
     else:
         logging.warning(f"Tentativa de acesso não autorizada: {usuario}")  # Log de acesso não autorizado
         return jsonify({'autorizado': False}), 403  # Forbidden
