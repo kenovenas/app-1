83eca9095d2e7e2eeefede74465d85e4698942cf
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-11-07 17:46:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ usuarios_autorizados = [
     "nbsbt", "mts", "jncmps", "dnln", "ok1698", "ok0091", 
     "ok0908", "ok2508", "ok2956", "ok1203", "ok9019", "ok4004", 
     "ok1999", "ok1982", "ok0198", "ok1001", "ok2090", "ok0902", 
-    "ok1516", "ok1119", "ok2324", "admin", "bete",
+    "ok1516", "ok1101", "ok2324", "admin", "bete",
 ]
 
 # Dicionário para rastrear visitas dos usuários
