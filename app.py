1554cb69868012045e87ce9217762a5a084ea09f
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-11-24 12:59:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Lista de usuários autorizados
 usuarios_autorizados = [
-     "ok1390", "ok1203", "ok9988", "mro", "ok9904",
+     "ok1390", "ok1203", "ok9988", "ok1999", "mro", "ok9904",
      "ok0091", "ok5178", "ok1880", "ok1001", "ok0908",
      "ok2508", "ok1698", "ok1698",
      "ok2090", "ok0902", 
